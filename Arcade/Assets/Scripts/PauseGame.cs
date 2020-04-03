7093cac2fdc3d937f2766c988f361e4121423a90
Footknight <timtnt11@gmail.com>
2020-04-03 17:31:43 -0600

    Implementing UI elements

---
+++ Arcade/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = false;
+        gamePaused = false;
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (gamePaused == false)
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+                Cursor.visible = true;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+                Cursor.visible = false;
+                gamePaused = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
---
+++ Arcade/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+ 
+
+    // Goto Arcade
+    public void GotoArcade()
+    {
+        SceneManager.LoadScene("Arcade");
+    }
+
+    //Goto HotDogGame
+    public void GotoHotDogGame()
+    {
+        SceneManager.LoadScene("HotDogGame");
+    }
+
+  
+}
