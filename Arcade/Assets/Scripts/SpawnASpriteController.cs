c2c82affa42242bd867da6985b52fd45f23a2d4e
Footknight <timtnt11@gmail.com>
2020-11-03 14:12:15 -0800

    Spawn A Sprite!
    Simple concept using the "shiny pokemon" logic

---
+++ Arcade/Assets/Scripts/SpawnASpriteController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnASpriteController : MonoBehaviour
+{
+    public GameObject Dice;
+    public GameObject ShinyDice;
+
+    // Spawn a die
+    public void SpawnDice()
+    {
+        if (Random.value > 0.01) //1% Spawn Chance
+        {
+            GameObject tmp = Instantiate(Dice);
+            tmp.transform.position = new Vector2(0, 0);
+        }
+        else
+        {
+            GameObject tmp = Instantiate(ShinyDice);
+            tmp.transform.position = new Vector2(0, 0);
+        }
+    }
+
+   
+}
